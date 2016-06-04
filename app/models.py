e3b042d52f9facda06136b3a13efde1e51bf9970
Paul Olteanu <p.a.olteanu@gmail.com>
2016-06-04 13:05:13 -0400

    Update model to match db

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from flask.ext.sqlalchemy import SQLAlchemy
+from random import randint
 db = SQLAlchemy()
 
 
@@ -6,10 +7,13 @@ class Photo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128))
     path = db.Column(db.String(128))
+    votes = db.Column(db.Integer, default=0)
+    created_on = db.Column(db.DateTime, server_default=db.func.now())
 
-    def __init__(self, title, path):
+    def __init__(self, title, path, votes=0):
         self.title = title
         self.path = path
+        self.votes = votes
 
     def __repr__(self):
         return '<Photo %r>' % self.title

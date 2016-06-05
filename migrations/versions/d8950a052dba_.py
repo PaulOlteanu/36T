c9ee7158329b54a6aad913019f8681d2e1dd391c
Paul Olteanu <p.a.olteanu@gmail.com>
2016-06-05 12:54:30 -0400

    Completely recreate initial migration. Exactly how you're not supposed to

--- migrations/versions/d8950a052dba_.py
+++ migrations/versions/d8950a052dba_.py
@@ -1,13 +1,13 @@
 """Create photo table
 
-Revision ID: 05eab60b5613
+Revision ID: d8950a052dba
 Revises: None
-Create Date: 2016-06-05 01:15:27.823835
+Create Date: 2016-06-05 12:52:42.367589
 
 """
 
 # revision identifiers, used by Alembic.
-revision = '05eab60b5613'
+revision = 'd8950a052dba'
 down_revision = None
 
 from alembic import op
@@ -16,12 +16,13 @@ import sqlalchemy as sa
 
 def upgrade():
     op.create_table('photo',
-        sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('id', sa.Integer(), nullable=False),
         sa.Column('title', sa.String(length=128), nullable=True),
         sa.Column('path', sa.String(length=128), nullable=True),
-        sa.Column('votes', sa.Integer(), nullable=True),
+        sa.Column('votes', sa.Integer(), nullable=False),
         sa.Column('created_on', sa.DateTime(), server_default=sa.text('now()'), nullable=True),
-        sa.PrimaryKeyConstraint('id')
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('path')
     )
 
 

157c91de534ef05794cbb56448100d14f5644823
Paul Olteanu <p.a.olteanu@gmail.com>
2016-06-14 09:29:39 -0400

    Add optional votes parameter to the Photo constructor

--- app/models.py
+++ app/models.py
@@ -10,10 +10,11 @@ class Photo(db.Model):
     votes = db.Column(db.Integer, nullable=False, default=0)
     created_on = db.Column(db.DateTime, server_default=db.func.now())
 
-    def __init__(self, title, filename, mimetype):
+    def __init__(self, title, filename, mimetype, votes=0):
         self.title = title
         self.filename = filename
         self.mimetype = mimetype
+        self.votes = votes
 
     def __repr__(self):
         return "<Photo ID: {}, Title: {}, Votes: {}, Creation_Date: {}>".format(self.id, self.title, self.votes, self.created_on)
